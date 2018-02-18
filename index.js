6c84d1b2304f175dfcdd7fb5b2ebbaaac9eab486
Arno van den Berg <arvandenberg@gmail.com>
2018-02-18 22:18:07 +0100

    nieuwe form fields en mongodb

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 /// START
 var express = require('express');
 var path = require('path')
+var bodyParser= require('body-parser')
 var app = express();        
 var port = process.env.PORT || 4000;
 
+app.use(bodyParser.urlencoded({extended: true}))
 
+var url = "mongodb://localhost:27017/bruiloft";
+
+const MongoClient = require('mongodb').MongoClient
+
+var db
+MongoClient.connect('mongodb://arno:bergvanden@ds141028.mlab.com:41028/bruiloft', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('bruiloft')
+  app.listen(port);
+	console.log('Magic happens on port ' + port);
+
+})
 app.use('/public', express.static(path.join(__dirname + "/public")));
 app.use('/vendor', express.static(path.join(__dirname + "/vendor")));
 
 
-//deze kan je veranderen in savethedate.html
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.post('/contact', (req, res) => {
+	db.collection('contact').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+	})
+})
